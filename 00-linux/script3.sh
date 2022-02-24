dab17893fb069aec76eb04965fe9f27c9d160a5c
Ana Huertas <ana.huertas.dominguez@gmail.com>
2022-02-24 13:38:57 +0100

    ej.3 completo

--- 00-linux/script3.sh
+++ 00-linux/script3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #$1 es el texto que vamos a escribir
+TEXTO=$1 #escribe lo que te hayan pasado
 
 #condicional por si no se pasa parametro
-if [[ $1==''ls
- ]]; then
-  $1="ta vacio"
+if [ -z "$TEXTO" ]; then #-z mira si esta vacio
+  TEXTO="Que me gusta la bash!!!!" #esta vacio, no te han pasado nada, escribe esto
 fi
 
 mkdir -p foo/dummy
@@ -13,6 +13,7 @@ cd foo
 mkdir empty
 cd dummy
 touch file1.txt file2.txt
-echo $1>file1.txt
+echo $TEXTO>file1.txt
+cd ..
 mv dummy/file2.txt empty
 cp dummy/file1.txt empty/file2.txt
